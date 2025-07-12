851ef698aa3839089ff5b3a75e51c42a5e229c8e
Somasundar S <somasundar.dev@gmail.com>
2025-07-12 20:16:26 +0530

    Update Github Action and S3 Bucket Folder in Terraform

--- Infra/s3.tf
+++ Infra/s3.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "upload_angular_app" {
   provisioner "local-exec" {
-    command = "aws s3 sync ../Docs/ s3://${data.aws_s3_bucket.website_bucket.id}/artifacts/${var.app_name}/${var.environment}/${var.current_version}/"
+    command = "aws s3 sync ./Docs/ s3://${data.aws_s3_bucket.website_bucket.id}/artifacts/${var.app_name}/${var.environment}/${var.current_version}/"
   }
 
   triggers = {
@@ -13,8 +13,8 @@ resource "aws_s3_object" "current_redirect" {
   bucket = data.aws_s3_bucket.website_bucket.id
   key    = "artifacts/${var.app_name}/${var.environment}/${var.current_version}/index.html"
 
-  source       = "../Docs/index.html"
-  etag         = filemd5("../Docs/index.html")
+  source       = "./Docs/index.html"
+  etag         = filemd5("./Docs/index.html")
   content_type = "text/html"
 
   depends_on = [null_resource.upload_angular_app]
